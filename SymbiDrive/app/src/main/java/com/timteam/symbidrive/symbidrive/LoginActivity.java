72e491bfc114df280132912c4dcee1907ca99a94
alexnivan <alexnivan@yahoo.com>
2015-03-21 22:47:26 +0200

    test push - removed settings from login page

--- SymbiDrive/app/src/main/java/com/timteam/symbidrive/symbidrive/LoginActivity.java
+++ SymbiDrive/app/src/main/java/com/timteam/symbidrive/symbidrive/LoginActivity.java
@@ -29,7 +29,7 @@ public class LoginActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+        //getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
 
